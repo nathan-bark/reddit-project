4e0c06e65490512ef28564e8bbdb4dbe495796b0
Nathan <nathanbark4@gmail.com>
2023-04-25 15:08:50 +0300

    Complete navbar basics + sign up button component

---
+++ src/Components/Buttons/SignInRegisterButton.js
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+const SignInRegisterButton = () => {
+  return (
+    <Button
+      variant="contained"
+      size="small"
+      sx={{ backgroundColor: "#666BB5" }}
+    >
+      Sign Up / Register
+    </Button>
+  );
+};
+export default SignInRegisterButton;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,20 +1,17 @@
 import "./Navbar.css";
 
-import Button from "@mui/material/Button";
+import SignInRegisterButton from "../Buttons/SignInRegisterButton";
+import logo from '../../assets/logo-no-background.png'
 
 const NavBar = () => {
   return (
     <div className="container">
       <nav className="navbar">
-        <img src="" alt="logo" className="logo" />
-        <input type="text" placeholder="Search" className="searchbar" />
-        <Button
-          variant="contained"
-          size="small"
-          sx={{ backgroundColor: "#666BB5" }}
-        >
-          Sign Up / Register
-        </Button>
+        <img src={logo} alt="logo" className="logo" />
+        <div className="nav-options">
+          <input type="text" placeholder="Search" className="searchbar" />
+          <SignInRegisterButton />
+        </div>
       </nav>
     </div>
   );
