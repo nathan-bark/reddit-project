792a76a38d5e1ba989f71c856eeea8c74cb7cdbf
Nathan <nathanbark4@gmail.com>
2023-04-28 18:38:03 +0300

    search page basic layout with subreddit cards

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -2,8 +2,9 @@ import SubRedditCard from "../SubRedditCards/SubRedditCards";
 
 const SearchPage = () => {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <div className=" w-9/12 p-20 bg-zinc-500 rounded-3xl flex">
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 className=" text-5xl mb-20"> Results for r/Lizard</h1>
+      <div className=" w-9/12 p-20 bg-zinc-500 rounded-3xl flex  justify-center flex-wrap">
         <SubRedditCard />
         <SubRedditCard />
         <SubRedditCard />
